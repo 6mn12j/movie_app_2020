2ea849d4a3cd7140ee22648ae6665117782844f5
6mn12j <dbwk0404@hanmail.net>
2020-12-08 01:50:35 +0900

    JSX,Props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 
+function Food({favorite}) {
+  console.log();
+  return <h1>I like {favorite}</h1>;
+}
+
 function App() {
   return (
-    <div className="App">Hello
+    <div>
+      <h1>Hello</h1>    
+      <Food favorite ="kimchi" />
+      <Food favorite ="ramen" />
+      <Food favorite ="sam" />
+      <Food favorite ="chukumi" />
     </div>
   );
 }
