947df25260969037b2aac93692933c284de72fb5
6mn12j <dbwk0404@hanmail.net>
2020-12-11 01:09:22 +0900

    Add HTML to  APP.js, Movie.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css"
 
 class App extends React.Component {
   state = {
@@ -24,25 +25,35 @@ class App extends React.Component {
 
   async componentDidMount() {
     this.getMovies();
-
   }
 
 
 
   render() {
     const { isLoading, movies } = this.state; //state 에서온 movies
-    return <div>{isLoading ? "Loading...." : movies.map(movie =>(
-      <Movie 
-        key = {movie.id} //key가 있어야 함
-        id = {movie.id} 
-        year = {movie.year} 
-        title = {movie.title} 
-        summary = {movie.summary} 
-        poster = {movie.medium_cover_image} 
-      />
-    ))}
-  </div>;
-  }
-}
+    return (
+      <section class="container">
+        {isLoading
+          ? <div class="loader">
+            <span class="loader__text">
+              "Loading..."
+            </span>
+          </div>
+          : <div class="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+                ))}
+            </div>}
+              </section>
+            );
+          }
+        }
 
 export default App;
